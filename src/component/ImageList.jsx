56978e0a4150055c1ffbf271d3ef7c8a1ac46c36
Jinnie <hjk7639@gmail.com>
2024-06-09 19:50:56 +0900

    Add: Passing an array of image search results to an ImageList component #4

--- src/component/ImageList.jsx
+++ src/component/ImageList.jsx
@@ -1,5 +1,11 @@
-function ImageList() {
-  return <div>Image List</div>;
+import ImageCard from './ImageCard';
+
+function ImageList({ imageLists }) {
+  const renderedImages = imageLists.map((image) => {
+    return <ImageCard image={image} key={image.id} />;
+  });
+
+  return <div>{renderedImages}</div>;
 }
 
 export default ImageList;
