86e5dba0a78efa5b052c38b22ff5ddad63668f11
Jinnie <hjk7639@gmail.com>
2024-06-09 15:21:02 +0900

    Add: Create an Image List state and prop function #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import ImageList from './component/ImageList';
 import SearchBar from './component/SearchBar';
+import searchImageList from './api';
 
 function App() {
+  const [imageLists, setImageLists] = useState([]);
+
+  const getImageList = async (term) => {
+    const result = await searchImageList(term);
+    searchImageList(result);
+  };
+
   return (
     <>
       <div>
-        <SearchBar />
+        <SearchBar onSubmit={getImageList} />
       </div>
       <div>
-        <ImageList />
+        <ImageList imageLists={imageLists} />
       </div>
     </>
   );
