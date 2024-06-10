f62b459bf97ced274385853a9698907cf2507e6a
Jinnie <hjk7639@gmail.com>
2024-06-10 21:48:51 +0900

    Add: Apply grid system to App component #6

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ function App() {
   };
 
   return (
-    <>
-      <div>
+    <section className="width-[1180px] height-[1080px] mx-auto grid gap-x-[20px] grid-cols-12 grid-rows-12">
+      <h1 className="col-start-2 col-span-2 row-span-2 text-[64px]">Search</h1>
+      <header className="row-start-4 col-start-2 col-span-10 row-span-1">
         <SearchBar onSubmit={getImageList} />
-      </div>
-      <div>
+      </header>
+      <main className="row-start-5 col-start-2 col-span-10">
         <ImageList imageLists={imageLists} />
-      </div>
-    </>
+      </main>
+    </section>
   );
 }
 
