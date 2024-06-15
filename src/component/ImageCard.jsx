27c68e4dc695324481ec74dcc788f4c78e0db2a6
Jinnie <hjk7639@gmail.com>
2024-06-15 11:27:25 +0900

    Add: Write a download code #8

--- src/component/ImageCard.jsx
+++ src/component/ImageCard.jsx
@@ -4,13 +4,15 @@ function ImageCard({ image }) {
       <img src={image.urls.small} alt={image.alt_description} className="rounded-lg mb-[20px]" />
       <div className="absolute bottom-1 right-1">
         <button type="button" className="p-1 mr-1 bg-white rounded-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-            />
-          </svg>
+          <a href={`${image.links.download}&force=true`} title="선택 이미지 다운로드">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </a>
         </button>
         <button type="button" className="p-1 bg-white rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
