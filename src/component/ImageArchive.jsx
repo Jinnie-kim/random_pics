4c236153807d33967ce952ff22734531d0fc174d
Jinnie <hjk7639@gmail.com>
2024-07-02 22:08:09 +0900

    Add: Make blueprint of ImageArchive component #10

---
+++ src/component/ImageArchive.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+function ImageArchive() {
+  const [imageLists, setImageLists] = useState([]);
+
+  return (
+    <section className="container m-auto px-3">
+      <div className="flex justify-between">
+        <h1 className="mt-[90px] text-[64px]">Favorite Pic Archive🎞️</h1>
+        <button className="mt-[90px]">
+          <Link to="/">Home🌌</Link>
+        </button>
+      </div>
+      <header className="mt-[40px]">
+        <SearchBar />
+      </header>
+
+      <main>
+        <ImageList imageLists={imageLists} />
+      </main>
+    </section>
+  );
+}
+
+export default ImageArchive;
