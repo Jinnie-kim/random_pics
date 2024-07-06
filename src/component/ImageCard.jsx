023b9b5e687e8fc8f117ddabf131e14f6bfcca26
Jinnie <hjk7639@gmail.com>
2024-07-06 12:00:53 +0900

    Add: Implement copy-to-clipboard feature for image URLs when share button is clicked #12

--- src/component/ImageCard.jsx
+++ src/component/ImageCard.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+import { PiHeartLight } from 'react-icons/pi';
+import { FcLike } from 'react-icons/fc';
+import { BsDownload, BsShare } from 'react-icons/bs';
 
 function ImageCard({ image }) {
   const [heartClick, setHeartClick] = useState(false);
@@ -27,6 +30,10 @@ function ImageCard({ image }) {
     }
   };
 
+  const copyImageUrlClipBoard = (url) => {
+    navigator.clipboard.writeText(url);
+  };
+
   useEffect(() => {
     const favPicsLists = JSON.parse(localStorage.getItem('favPics')) || [];
 
@@ -43,38 +50,28 @@ function ImageCard({ image }) {
     <li className="relative">
       <img src={image.urls.small} alt={image.alt_description} className="rounded-lg mb-[20px]" />
       <div className="absolute bottom-1 right-1">
-        <button type="button" className="p-1 mr-1 bg-white rounded-full">
+        <button
+          type="button"
+          className="p-1 mr-1 bg-rose-50 rounded"
+          onClick={() => {
+            copyImageUrlClipBoard(image.urls.small);
+          }}
+        >
+          <BsShare />
+        </button>
+        <button type="button" className="p-1 mr-1 bg-rose-50 rounded">
           <a href={`${image.links.download}&force=true`} title="선택 이미지 다운로드">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-              />
-            </svg>
+            <BsDownload />
           </a>
         </button>
         <button
           type="button"
-          className="p-1 bg-white rounded-full"
+          className="p-1 bg-rose-50 rounded"
           onClick={() => {
             addFavoritePics(image);
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill={heartClick ? 'red' : 'white'}
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-            />
-          </svg>
+          {heartClick ? <FcLike /> : <PiHeartLight />}
         </button>
       </div>
     </li>
