cd610212d755ba1e223a0a8e28a8800491c106c8
Jinnie <hjk7639@gmail.com>
2024-07-14 14:21:19 +0900

    Feat: Apply combined context provider #21

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import CombinedProviders from './hooks/use-combined-context';
 import App from './App';
 import './index.css';
-import ImageArchive from './component/ImageArchive';
-import { Provider } from './context/favPicLists';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/archive',
-    element: <ImageArchive />,
-  },
-]);
-
 root.render(
   <React.StrictMode>
-    <Provider>
-      <RouterProvider router={router} />
-    </Provider>
+    <CombinedProviders>
+      <App />
+    </CombinedProviders>
   </React.StrictMode>
 );
