670cb9f30e7fe49c3cbeb8b4c5fd46fa5ce21b12
Jinnie <hjk7639@gmail.com>
2024-07-14 14:35:40 +0900

    Refactor: Extract existing content into separate files and register routes in App #21

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import ImageList from './component/ImageList';
-import SearchBar from './component/SearchBar';
-import searchImageList from './api';
+import Route from './component/Route';
+import ImageArchive from './page/ImageArchivePage';
+import ImageSearch from './page/ImageSearchPage';
 
 function App() {
-  const [imageLists, setImageLists] = useState([]);
-
-  const getImageList = async (term) => {
-    const result = await searchImageList(term);
-    setImageLists(result);
-  };
-
   return (
-    <section className="container m-auto px-3">
-      <div className="flex justify-between">
-        <h1 className="mt-[90px] text-[64px]">Search</h1>
-        <button className="mt-[90px]">
-          <Link to="/archive">Archive🎞️</Link>
-        </button>
-      </div>
-      <header className="mt-[40px]">
-        <SearchBar onSubmit={getImageList} />
-      </header>
-
-      <main>
-        <ImageList imageLists={imageLists} />
-      </main>
-    </section>
+    <div>
+      <Route path="/">
+        <ImageSearch />
+      </Route>
+      <Route path="/archive">
+        <ImageArchive />
+      </Route>
+    </div>
   );
 }
 
