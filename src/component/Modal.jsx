86c54c2c070482689e4f594f57265d0828d41252
Jinnie <hjk7639@gmail.com>
2024-07-16 20:41:19 +0900

    Fix: Update Modal component CSS for improved layout and styling #23

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -4,9 +4,9 @@ function Modal({ children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div className="fixed inset-0 bg-gray-200 opacity-80"></div>
-      <div className="fixed top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] p-10 w-[350px] h-[300px] rounded-xl bg-white flex flex-col justify-between">
-        {children}
-        <div className="flex justify-end">{actionBar}</div>
+      <div className="fixed top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] p-10 w-[350px] h-[300px] rounded-xl bg-white flex flex-col justify-between items-center">
+        <div>{children}</div>
+        <div className="w-full">{actionBar}</div>
       </div>
     </div>,
     document.querySelector('.modal-container')
