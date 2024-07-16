47bf5d9050046c1fc0e24a1991ec6338584f6b66
Jinnie <hjk7639@gmail.com>
2024-07-16 21:35:27 +0900

    Feat: Add confirmation modal to ImageArchive page for data reset #24

--- src/page/ImageArchivePage.jsx
+++ src/page/ImageArchivePage.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react';
 import useFavPicListsContext from '../hooks/use-favPicLists-context';
 import Link from '../component/Link';
 import ImageList from '../component/ImageList';
+import Modal from '../component/Modal';
 
 function ImageArchive() {
   const { imageLists, existFavPicLists } = useFavPicListsContext();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
 
   const resetFavPicLists = () => {
     localStorage.removeItem('favPics');
     existFavPicLists();
+    setShowModal(false);
   };
+
+  const actionBar = (
+    <div className="flex justify-around">
+      <button
+        className="w-[60px] h-[30px] border-2 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium"
+        onClick={resetFavPicLists}
+      >
+        예
+      </button>
+      <button
+        className="w-[60px] h-[30px] border-2 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium"
+        onClick={handleModalClose}
+      >
+        아니오
+      </button>
+    </div>
+  );
+
+  const modal = (
+    <Modal actionBar={actionBar}>
+      <h1>아카이브를 초기화하시겠습니까?</h1>
+    </Modal>
+  );
 
   return (
     <section className="container m-auto px-3">
@@ -20,11 +55,12 @@ function ImageArchive() {
       </div>
 
       <main className="flex flex-col">
-        <button className="ml-auto border-2 p-1 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium" onClick={resetFavPicLists}>
+        <button className="ml-auto border-2 p-1 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium" onClick={handleModalOpen}>
           Reset Archive
         </button>
         <>{imageLists.length > 0 ? <ImageList imageLists={imageLists} /> : <h1 className="mx-auto mt-[65px]">You haven't liked any photos yet!</h1>}</>
       </main>
+      {showModal && modal}
     </section>
   );
 }
