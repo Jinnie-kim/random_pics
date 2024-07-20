284c95e4df58e39ec3270578fa9dc831da999651
Jinnie <hjk7639@gmail.com>
2024-07-20 15:55:01 +0900

    Feat: Add reusable common Button component #26

---
+++ src/component/Button.jsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames';
+
+function Button({ children, text, icon, ...rest }) {
+    const classes = classNames(rest.className, 'bg-rose-50', {
+        'w-[60px] h-[30px] border-2 rounded-xl border-rose-500 text-gray-400 hover:text-rose-500 font-medium':
+            text,
+        'p-1 mr-1 rounded': icon,
+    });
+
+    return (
+        <button type="button" {...rest} className={classes}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
