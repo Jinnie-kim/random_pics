74ccbc11a4d573439f8492a07151a7ea413c7a47
Jinnie <hjk7639@gmail.com>
2024-07-20 16:21:34 +0900

    Refactor: Replace button tags with Button component on ImageArchive #26

--- src/page/ImageArchivePage.jsx
+++ src/page/ImageArchivePage.jsx
@@ -3,66 +3,61 @@ import useFavPicListsContext from '../hooks/use-favPicLists-context';
 import Link from '../component/Link';
 import ImageList from '../component/ImageList';
 import Modal from '../component/Modal';
+import Button from '../component/Button';
 
 function ImageArchive() {
-  const { imageLists, existFavPicLists } = useFavPicListsContext();
-  const [showModal, setShowModal] = useState(false);
+    const { imageLists, existFavPicLists } = useFavPicListsContext();
+    const [showModal, setShowModal] = useState(false);
 
-  const handleModalOpen = () => {
-    setShowModal(true);
-  };
+    const handleModalOpen = () => {
+        setShowModal(true);
+    };
 
-  const handleModalClose = () => {
-    setShowModal(false);
-  };
+    const handleModalClose = () => {
+        setShowModal(false);
+    };
 
-  const resetFavPicLists = () => {
-    localStorage.removeItem('favPics');
-    existFavPicLists();
-    setShowModal(false);
-  };
+    const resetFavPicLists = () => {
+        localStorage.removeItem('favPics');
+        existFavPicLists();
+        setShowModal(false);
+    };
 
-  const actionBar = (
-    <div className="flex justify-around">
-      <button
-        className="w-[60px] h-[30px] border-2 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium"
-        onClick={resetFavPicLists}
-      >
-        예
-      </button>
-      <button
-        className="w-[60px] h-[30px] border-2 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium"
-        onClick={handleModalClose}
-      >
-        아니오
-      </button>
-    </div>
-  );
+    const actionBar = (
+        <div className="flex justify-around">
+            <Button text onClick={resetFavPicLists}>
+                예
+            </Button>
+            <Button text onClick={handleModalClose}>
+                아니오
+            </Button>
+        </div>
+    );
 
-  const modal = (
-    <Modal actionBar={actionBar}>
-      <h1>아카이브를 초기화하시겠습니까?</h1>
-    </Modal>
-  );
+    const modal = (
+        <Modal actionBar={actionBar}>
+            <h1>아카이브를 초기화하시겠습니까?</h1>
+        </Modal>
+    );
 
-  return (
-    <section className="container m-auto px-3">
-      <div className="flex justify-between">
-        <h1 className="mt-[90px] text-[64px]">Favorite Pic Archive🎞️</h1>
-        <button className="mt-[90px]">
-          <Link to="/">Home🌌</Link>
-        </button>
-      </div>
+    return (
+        <section className="container m-auto px-3">
+            <div className="flex justify-between">
+                <h1 className="mt-[90px] text-[64px]">Favorite Pic Archive🎞️</h1>
+                <button type="button" className="mt-[90px]">
+                    <Link to="/">Home🌌</Link>
+                </button>
+            </div>
 
-      <main className="flex flex-col">
-        <button className="ml-auto border-2 p-1 rounded-xl bg-rose-50 border-rose-500 text-gray-400 hover:text-rose-500 font-medium" onClick={handleModalOpen}>
-          Reset Archive
-        </button>
-        <>{imageLists.length > 0 ? <ImageList imageLists={imageLists} /> : <h1 className="mx-auto mt-[65px]">You haven't liked any photos yet!</h1>}</>
-      </main>
-      {showModal && modal}
-    </section>
-  );
+            <main className="flex flex-col">
+                <Button text className="w-fit h-fit ml-auto p-1 d" onClick={handleModalOpen}>
+                    Reset Archive
+                </Button>
+                <>{imageLists.length > 0 ? <ImageList imageLists={imageLists} /> : <h1 className="mx-auto mt-[65px]">You haven't liked any photos yet!</h1>}</>
+            </main>
+            {showModal && modal}
+        </section>
+    );
 }
 
 export default ImageArchive;
